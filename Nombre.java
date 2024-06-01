8aa0929343e45c3163f9745834f8e42d9d73d478
eliesMadraz <elye-elye23@hotmail.fr>
2024-06-02 01:25:09 +0200

    test

---
+++ Nombre.java
@@ -0,0 +1,19 @@
+
+
+public class Nombre extends Expression{
+    private int valeurNombre;
+    public Nombre(double valeur)
+    {
+        this.valeurNombre = valeur;
+    }
+
+    public double valeur()
+    {
+        return this.valeurNombre;
+    }
+
+    public String toString()
+    {
+        return "" + this.valeurNombre;
+    }
+}
